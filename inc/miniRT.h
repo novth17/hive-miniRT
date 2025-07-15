6940342c5843f6bfbfc68604abff6059a8969ee3
novth17 <nh.hiennguyen@gmail.com>
2025-07-15 20:46:39 +0300

    refix the main and starting point

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -6,13 +6,16 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 20:30:17 by hiennguy          #+#    #+#             */
-/*   Updated: 2025/07/15 20:32:55 by hiennguy         ###   ########.fr       */
+/*   Updated: 2025/07/15 20:46:13 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef MINI_RT_H
 #define MINI_RT_H
+
+#define SUCCESS	0
+#define FAIL	1
 
 #include <unistd.h>
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 20:30:30 by hiennguy          #+#    #+#             */
-/*   Updated: 2025/07/15 20:37:08 by hiennguy         ###   ########.fr       */
+/*   Updated: 2025/07/15 20:45:09 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	main(int argc, char **argv)
 	(void) **argv;
 	if (argc != 2)
 	{
-		write(1, "hihihaha", 8);
+		write(1, "oh no, try again\n", 17);
+		return (FAIL);
 	}
-	write(1, "hihihaha", 8);
-	return (0);
+	write(1, "hihihaha\n", 8);
+	return (SUCCESS);
 }
