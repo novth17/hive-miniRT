f5ff942fd6d48dc7e6d1e89ef8ebb668f9e9cfa7
novth17 <nh.hiennguyen@gmail.com>
2025-07-29 16:33:40 +0300

    fix parsing part

--- src/parser/identifier/object.c
+++ src/parser/identifier/object.c
@@ -1,8 +1,10 @@
 #include "mini_rt.h"
 
-static int parse_material(char **tokens, int offset, t_scene *scene, t_material *m);
+#include "mini_rt.h"
 
-static t_object	*create_objects(t_scene *scene, t_obj_type type)
+static int parse_material(char **tokens, t_scene *scene, t_material *m, int offset);
+
+static t_object *create_objects(t_scene *scene, t_obj_type type)
 {
 	t_object *obj;
 
@@ -13,7 +15,7 @@ static t_object	*create_objects(t_scene *scene, t_obj_type type)
 		scene->is_valid = false;
 		return (NULL);
 	}
- 	obj->type = type;
+	obj->type = type;
 	obj->next = scene->objects;
 	scene->objects = obj;
 	return (obj);
@@ -30,12 +32,12 @@ int parse_sphere(char **tokens, t_scene *scene)
 		return (FAIL);
 	object->sphere.center = parse_vec3(tokens[1], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Sphere: center: "ERROR_COORD, tokens[1]));
+		return (print_error("Sphere: center: " ERROR_COORD, tokens[1]));
 	object->sphere.radius = parse_float(tokens[2], &scene->is_valid) / 2;
 	if (!scene->is_valid)
-		return (print_error("Sphere: diameter: "ERROR_FLOAT, tokens[2]));
-	if (parse_material(tokens, scene, &scene->is_valid, 3))
-		return (print_error(RED"Sphere\n", NULL));
+		return (print_error("Sphere: diameter: " ERROR_FLOAT, tokens[2]));
+	if (parse_material(tokens, scene, &object->sphere.material, 3))
+		return (print_error(RED "Sphere\n", NULL));
 	return (SUCCESS);
 }
 
@@ -50,16 +52,16 @@ int parse_plane(char **tokens, t_scene *scene)
 		return (FAIL);
 	object->pl.point = parse_vec3(tokens[1], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Plane: point: "ERROR_COORD, tokens[1]));
+		return (print_error("Plane: point: " ERROR_COORD, tokens[1]));
 	object->pl.axis = parse_vec3(tokens[2], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Plane: axis: "ERROR_COORD, tokens[2]));
+		return (print_error("Plane: axis: " ERROR_COORD, tokens[2]));
 	if (!is_in_range_vec3(object->pl.axis))
-		return (print_error("Plane: axis: "ERROR_IN_RANGE, tokens[2]));
-	if(!is_normalized(object->pl.axis))
-		return (print_error("Plane: axis: "ERROR_NORM, tokens[2]));
-	if (parse_material(tokens, scene, &scene->is_valid, 3))
-		return (print_error(RED"Plane\n", NULL));
+		return (print_error("Plane: axis: " ERROR_IN_RANGE, tokens[2]));
+	if (!is_normalized(object->pl.axis))
+		return (print_error("Plane: axis: " ERROR_NORM, tokens[2]));
+	if (parse_material(tokens, scene, &object->pl.material, 3))
+		return (print_error(RED "Plane\n", NULL));
 	return (SUCCESS);
 }
 
@@ -67,49 +69,49 @@ int parse_cyl(char **tokens, t_scene *scene)
 {
 	t_object *object;
 
-	if (check_id_args_count(tokens, "Cylinder", 8) == FAIL)
+	if (check_id_args_count(tokens, "Cylinder", 9) == FAIL)
 		return (FAIL);
 	object = create_objects(scene, CYLINDER);
 	if (!object)
 		return (FAIL);
 	object->cyl.center = parse_vec3(tokens[1], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Cylinder: center: "ERROR_COORD, tokens[1]));
+		return (print_error("Cylinder: center: " ERROR_COORD, tokens[1]));
 	object->cyl.axis = parse_vec3(tokens[2], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Cylinder: axis: "ERROR_COORD, tokens[2]));
+		return (print_error("Cylinder: axis: " ERROR_COORD, tokens[2]));
 	if (!is_in_range_vec3(object->cyl.axis))
-		return (print_error("Cylinder: axis: "ERROR_IN_RANGE, tokens[2]));
-	if(!is_normalized(object->pl.axis))
-		return (print_error("Cylinder: axis: "ERROR_NORM, tokens[2]));
+		return (print_error("Cylinder: axis: " ERROR_IN_RANGE, tokens[2]));
+	if (!is_normalized(object->cyl.axis))
+		return (print_error("Cylinder: axis: " ERROR_NORM, tokens[2]));
 	object->cyl.diameter = parse_float(tokens[3], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Cylinder: diameter: "ERROR_FLOAT, tokens[3]));
+		return (print_error("Cylinder: diameter: " ERROR_FLOAT, tokens[3]));
 	object->cyl.height = parse_float(tokens[4], &scene->is_valid);
 	if (!scene->is_valid)
-		return (print_error("Cylinder: height: "ERROR_FLOAT, tokens[4]));
-	if (parse_material(tokens, scene, &scene->is_valid, 5))
-		return (print_error(RED"Cylinder\n", NULL));
+		return (print_error("Cylinder: height: " ERROR_FLOAT, tokens[4]));
+	if (parse_material(tokens, scene, &object->cyl.material, 5))
+		return (print_error(RED "Cylinder\n", NULL));
 	return (SUCCESS);
 }
-static int parse_material(char **tokens, int offset, t_scene *scene, t_material *m)
+
+static int parse_material(char **tokens, t_scene *scene, t_material *m, int offset)
 {
 	m->color = parse_color(tokens[offset], &scene->is_valid);
 	if (!scene->is_valid)
-		return print_error("Material: "ERROR_COLOR, tokens[offset]);
+		return print_error("Material: " ERROR_COLOR, tokens[offset]);
 
 	m->diffuse = parse_float(tokens[offset + 1], &scene->is_valid);
 	if (!scene->is_valid)
-		return print_error("Material: diffuse: "ERROR_FLOAT, tokens[offset + 1]);
+		return print_error("Material: diffuse: " ERROR_FLOAT, tokens[offset + 1]);
 
 	m->specular_probability = parse_float(tokens[offset + 2], &scene->is_valid);
 	if (!scene->is_valid)
-		return print_error("Material: specular: "ERROR_FLOAT, tokens[offset + 2]);
+		return print_error("Material: specular: " ERROR_FLOAT, tokens[offset + 2]);
 
 	m->emitter = parse_float(tokens[offset + 3], &scene->is_valid);
 	if (!scene->is_valid)
-		return print_error("Material: emitter: "ERROR_FLOAT, tokens[offset + 3]);
+		return print_error("Material: emitter: " ERROR_FLOAT, tokens[offset + 3]);
 
 	return (SUCCESS);
 }
-
