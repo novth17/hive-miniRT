458045602c9e96e5e506454daafc977b8e777086
novth17 <nh.hiennguyen@gmail.com>
2025-07-29 18:37:54 +0300

    start doing plane

---
+++ src/plane/render.c
@@ -0,0 +1,22 @@
+#include "mini_rt.h"
+
+//define intersection
+bool hit_plane(t_ray *ray, t_plane *plane)
+{
+
+}
+
+
+t_hit create_plane_hit_record(const t_ray ray, const t_sphere sp, const float root)
+{
+	t_hit rec;
+
+
+	return (rec);
+}
+
+float plane_hit(const t_plane pl, const t_ray ray)
+{
+
+
+}
