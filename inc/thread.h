47ba26927de306ad87da8205e987a00490101e61
novth17 <nh.hiennguyen@gmail.com>
2025-08-29 17:02:53 +0300

    remove comment on thread.h

--- inc/thread.h
+++ inc/thread.h
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   thread.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltaalas <ltaalas@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 16:29:15 by ltaalas           #+#    #+#             */
-/*   Updated: 2025/08/29 16:29:27 by ltaalas          ###   ########.fr       */
+/*   Updated: 2025/08/29 17:02:38 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef THREAD_H
 # define THREAD_H
-# include "mini_rt.h" //wtf why need here?
+# include "mini_rt.h"
 
 # ifdef MINIRT_BONUS
 
